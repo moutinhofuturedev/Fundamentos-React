0933b501bfa3f2880a440c7e300317fda2e9ffd5
Paulo Moutinho <paulo_vicali@icloud.com>
2022-07-21 18:16:12 -0300

    add teacher profile

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -30,6 +30,14 @@ function Home() {
   return(
     <div className='container'>
       <form onSubmit={Submit}>
+        <header>
+          <img src="https://github.com/moutinhofuturedev.png" alt="Imagem de perfil do professor" />
+          <div>
+            <strong>Paulo Moutinho</strong>
+            <p>Front-end Software Enginner</p>
+          </div>
+        </header>
+
         <h1>Lista de presença</h1>
         <input type="text" placeholder='digite seu nome' onChange={event => setStudentName(event.target.value)}/>
 
@@ -39,6 +47,10 @@ function Home() {
       <section>
         {students.map(element => { // percorrer uma lista para que a cada renderização eu tenha mais um componente com um novo dado
           return <Card key={element} text={element} time={getTime()}/>
+          // KEY PROP
+          // Em uma listagem, normalmente utilizamos o map() do JavaScript para trazer todos os dados dessa lista. 
+          // No React, precisamos passar uma propriedade key para que esse dado nunca se repita e evitar que erros desse tipo aconteçam.
+          // => aconselhavel sempre usar 'id' (key.id)
         })}
       </section> 
     </div>
